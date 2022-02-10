132eeac54513f389179af6e20ad9708238a2f05f
Hanane-Hadjab <71149750+Hanane-Hadjab@users.noreply.github.com>
2022-02-10 13:12:14 +0100

    Update file

--- rangement.py
+++ rangement.py
@@ -1,47 +1,61 @@
 import os
-import sys
 import magic
 import stat
 import shutil
+import argparse
 
 
-def move(folder_src):
-    for filename in os.listdir(folder_to_track):
+def move(folder_src, extension_arg):
+    for filename in os.listdir(folder_src):
 
         filename_path = folder_src + '/' + filename
-
         file_stats = os.stat(filename_path)
 
         if stat.S_ISDIR(file_stats[stat.ST_MODE]):
-            print("This is directory" + filename)
+            print(filename + " is a directory")
             continue
 
-        # Search type mims of file
-        type_mime = magic.Magic(mime=True, uncompress=True)
-        type_file = type_mime.from_file(filename_path)
-
-        extension_file = os.path.split(type_file)[1]
-
-        folder = folder_to_track + "/" + "Dossier_" + extension_file
-
-        # If folder exist insert
-        # sinon create the folder and insert
-        if os.path.isdir(folder):
-            src = folder_src + "/" + filename
-            new_destination = folder + "/" + filename
-            shutil.move(src, new_destination)
+        if extension_arg is None:
+            # Search type mims of file
+            type_mime = magic.Magic(mime=True, uncompress=True)
+            type_file = type_mime.from_file(filename_path)  # exemple application/pdf
+            extension_file = os.path.split(type_file)[1]  # exemple pdf
         else:
+            extension_file = extension_arg
+
+        folder = folder_src + "/" + "Dossier_" + extension_file
+
+        # If folder exist  then insert file in it
+        if os.path.isdir(folder):
+            if extension_arg is not None:
+                if filename.endswith('.'+extension_arg):
+                    src = folder_src + "/" + filename
+                    new_destination = folder + "/" + filename
+                    shutil.move(src, new_destination)
+            else:
+                src = folder_src + "/" + filename
+                new_destination = folder + "/" + filename
+                shutil.move(src, new_destination)
+        else:
+            # sinon create the folder and insert
             # create Directory
             path = os.path.join(folder_src, folder)
-            print(path)
-            os.mkdir(path)
-            src = folder_src + "/" + filename
-            new_destination = path + "/" + filename
-            shutil.move(src, new_destination)
+            if extension_arg is not None:
+                if filename.endswith('.'+extension_arg):
+                    os.mkdir(path)
+                    src = folder_src + "/" + filename
+                    new_destination = path + "/" + filename
+                    shutil.move(src, new_destination)
+            else:
+                os.mkdir(path)
+                src = folder_src + "/" + filename
+                new_destination = path + "/" + filename
+                shutil.move(src, new_destination)
 
 
-if len(sys.argv) == 1:
-    print("Missing argument")
-else:
-    folder_to_track = sys.argv[1]
-    move(folder_to_track)
+parser = argparse.ArgumentParser()
+parser.add_argument('--path', type=str, required=True, help="Le chemin de dossier cible")
+parser.add_argument('--only', type=str, help='L extension de fichier a rangé ')
+args = parser.parse_args()
+
+move(args.path, args.only)
